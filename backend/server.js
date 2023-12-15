9896e1989a4c1028d0559101154a2d3d2ce9f676
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-12-15 08:03:59 +0100

    added error handling to backend

--- backend/server.js
+++ backend/server.js
@@ -18,51 +18,68 @@ const courseSchema = new mongoose.Schema({
     bg: String,
     bgPosition: String,
     color: String,
-    tasks: [
-        {
-            text: String,
-            date: String,
-            icon: String
-        }
-    ]
+    // tasks: [{task: Schema.ObjectId, ref: 'Task'}]
 
 });
 
-const Course = mongoose.model('Course', courseSchema);
+// const taskSchema = new mongoose.Schema(
+//     {
+//         text: String,
+//         date: String,
+//         icon: String
+//     }
+// );
 
+const Course = mongoose.model('Course', courseSchema);
+//const Task = mongoose.model('Task', taskSchema);
 // Routes
 app.get('/api/courses', async (req, res) => {
-    const courses = await Course.find();
-    res.json(courses);
+    try {
+        const courses = await Course.find();
+        res.json(courses);
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'Internal server error'});
+    }
 });
 app.get('/api/courses/:id', async (req, res) => {
     const courseId = req.params.id;
-
     try {
         // Find the course by ID
         const course = await Course.findById(courseId);
 
         if (!course) {
-            return res.status(404).json({ message: 'Course not found' });
+            return res.status(404).json({message: 'Course not found'});
         }
 
         // Display the course details
         res.json(course);
     } catch (error) {
         console.error('Error finding course:', error.message);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({message: 'Internal server error'});
     }
 });
 app.post('/api/courses', async (req, res) => {
-    const newCourse = new Course(req.body);
-    await newCourse.save();
-    res.json(newCourse);
+    try {
+        const newCourse = new Course(req.body);
+        await newCourse.save();
+        res.json(newCourse);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
 });
 
+
 app.delete('/api/courses/:id', async (req, res) => {
-    const { id } = req.params;
-    await Course.findByIdAndDelete(id);
-    res.json({ message: 'Course deleted' });
+    try {
+        const {id} = req.params;
+        await Course.findByIdAndDelete(id);
+        res.json({message: 'Course deleted'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
 });
 
 
