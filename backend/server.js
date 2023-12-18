08f4c219cd2a9161b09023ba9491a83c77d1a5e5
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-12-18 05:04:49 +0100

    deleted backend node_modules

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import cors from 'cors';
 
 const app = express();
@@ -11,27 +11,30 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.8yqyuwo.mongodb.net/?retryW
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-// Define a simple model
+
+const taskSchema = new mongoose.Schema(
+    {
+        text: String,
+        date: String,
+        icon: String
+    }
+);
+const Task = mongoose.model('Task', taskSchema);
+
+
 const courseSchema = new mongoose.Schema({
     teacher: String,
     groupName: String,
     bg: String,
     bgPosition: String,
     color: String,
-    // tasks: [{task: Schema.ObjectId, ref: 'Task'}]
-
+    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
 });
 
-// const taskSchema = new mongoose.Schema(
-//     {
-//         text: String,
-//         date: String,
-//         icon: String
-//     }
-// );
+
+// Define a simple model
 
 const Course = mongoose.model('Course', courseSchema);
-//const Task = mongoose.model('Task', taskSchema);
 // Routes
 app.get('/api/courses', async (req, res) => {
     try {
@@ -45,20 +48,21 @@ app.get('/api/courses', async (req, res) => {
 app.get('/api/courses/:id', async (req, res) => {
     const courseId = req.params.id;
     try {
-        // Find the course by ID
-        const course = await Course.findById(courseId);
+        // Find the course by ID and populate the 'tasks' field
+        const course = await Course.findById(courseId).populate('tasks').exec();
 
         if (!course) {
             return res.status(404).json({message: 'Course not found'});
         }
 
-        // Display the course details
+        // Display the course details with populated tasks
         res.json(course);
     } catch (error) {
         console.error('Error finding course:', error.message);
         res.status(500).json({message: 'Internal server error'});
     }
 });
+
 app.post('/api/courses', async (req, res) => {
     try {
         const newCourse = new Course(req.body);
@@ -66,6 +70,32 @@ app.post('/api/courses', async (req, res) => {
         res.json(newCourse);
     } catch (error) {
         console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+});
+app.post('/api/courses/:id/tasks', async (req, res) => {
+    const courseId = req.params.id;
+    const taskDetails = req.body;
+
+    try {
+        // Check if the course exists
+        const course = await Course.findById(courseId);
+
+        if (!course) {
+            return res.status(404).json({message: 'Course not found'});
+        }
+
+        // Create a new task
+        const newTask = new Task(taskDetails);
+        await newTask.save();
+
+        // Add the task to the course's tasks array
+        course.tasks.push(newTask._id);
+        await course.save();
+
+        res.json({course, newTask});
+    } catch (error) {
+        console.error('Error adding task to course:', error.message);
         res.status(500).json({message: 'Internal server error'});
     }
 });
