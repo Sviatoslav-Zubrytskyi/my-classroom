87bceefe59449650b4515051be88b5240eb9442c
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-12-25 06:35:47 +0100

    added dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import courseRoutes from "./routes/courseRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
-const port = 5050;
+dotenv.config();
+const {PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_ID} = process.env
+
 app.use(cors());
 app.use(express.json());
 
@@ -13,13 +16,11 @@ app.use('/api/courses', courseRoutes);
 app.use('/api/courses', taskRoutes);
 const start = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin:admin@cluster0.8yqyuwo.mongodb.net/?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.${DB_ID}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
 
-        app.listen(port, () => {
-            console.log('Server start port ' + port)
+        app.listen(PORT, () => {
+            console.log('Server start port ' + PORT);
         })
     } catch (err) {
         console.error(err)
